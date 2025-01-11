c80c0a7b09fbe5e2c9d968b4a81fe29fe54d2b42
AkshayKumar41 <akskum700@gmail.com>
2025-01-10 23:55:33 -0500

    Works, but can't prefire commands!!!!!

--- ThriftyTest/src/main/java/frc/robot/RobotContainer.java
+++ ThriftyTest/src/main/java/frc/robot/RobotContainer.java
@@ -14,11 +14,14 @@ import org.slf4j.LoggerFactory;
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.TunerConstants;
@@ -47,7 +50,7 @@ public class RobotContainer {
     public RobotContainer() {
         configureBindings();
 
-        boolean isCompetition = true;
+        // boolean isCompetition = true;
 
         // Filter any autos with the "comp" prefix
         // autoChooser = AutoBuilder.buildAutoChooserWithOptionsModifier(
@@ -90,9 +93,35 @@ public class RobotContainer {
         joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
         drivetrain.registerTelemetry(logger::telemeterize);
+
+        joystick.button(1).onTrue(new InstantCommand(() -> drivetrain.sequenceOnTheFlyPaths(ScoringLocations.LOWERHP.value)));
+        joystick.button(2).onTrue(new InstantCommand(() -> drivetrain.sequenceOnTheFlyPaths(ScoringLocations.UPPERHP.value)));
+
+        joystick.axisMagnitudeGreaterThan(0, 0.0).or(() -> joystick.axisMagnitudeGreaterThan(1, 0.0).getAsBoolean()).onTrue(new InstantCommand(() -> drivetrain.resetOnTheFly()));
+
     }
 
     public Command getAutonomousCommand() {
         return autoChooser.getSelected();
+    }
+
+    public enum ScoringLocations {
+        A("ALineup"),
+        B("BLineup"),
+        C("CLineup"),
+        D("DLineup"),
+        E("ELineup"),
+        F("FLineup"),
+        G("GLineup"),
+        H("HLineup"),
+        LOWERHP("LowerHPLineup"),
+        UPPERHP("UpperHPLineup"),
+        MIDDLE("MiddleLineup");
+
+        private String value;
+
+        private ScoringLocations(String value) {
+            this.value = value;
+        }
     }
 }
