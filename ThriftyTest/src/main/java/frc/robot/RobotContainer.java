d6d24e9447538ba417c784cf3585404cc1e718b4
Akshay Kumar <91789994+AkshayKumar41@users.noreply.github.com>
2025-01-15 20:39:37 -0500

    got poses for autonomous on the fly stuff. gotta make the actual command stuff work

--- ThriftyTest/src/main/java/frc/robot/RobotContainer.java
+++ ThriftyTest/src/main/java/frc/robot/RobotContainer.java
@@ -16,12 +16,12 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
-import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.RobotContainer.ScoringLocations;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
@@ -45,6 +45,10 @@ public class RobotContainer {
 
     private final SendableChooser<Command> autoChooser;
 
+    private final SendableChooser<ScoringLocations> scoringFirst;
+    private final SendableChooser<ScoringLocations> scoringSecond;
+    private final SendableChooser<ScoringLocations> scoringThird;
+
     public RobotContainer() {
         configureBindings();
 
@@ -59,8 +63,50 @@ public class RobotContainer {
 
         autoChooser = AutoBuilder.buildAutoChooser();
         
-
         SmartDashboard.putData("Auto Chooser", autoChooser);
+
+        scoringFirst = new SendableChooser<>();
+        scoringFirst.addOption("A", ScoringLocations.A);
+        scoringFirst.addOption("B", ScoringLocations.B);
+        scoringFirst.addOption("C", ScoringLocations.C);
+        scoringFirst.addOption("D", ScoringLocations.D);
+        scoringFirst.addOption("E", ScoringLocations.E);
+        scoringFirst.addOption("F", ScoringLocations.F);
+        scoringFirst.addOption("G", ScoringLocations.G);
+        scoringFirst.addOption("H", ScoringLocations.H);
+        scoringFirst.addOption("I", ScoringLocations.I);
+        scoringFirst.addOption("J", ScoringLocations.J);
+        scoringFirst.addOption("K", ScoringLocations.K);
+        scoringFirst.addOption("L", ScoringLocations.L);
+
+        scoringSecond = new SendableChooser<>();
+        scoringSecond.addOption("A", ScoringLocations.A);
+        scoringSecond.addOption("B", ScoringLocations.B);
+        scoringSecond.addOption("C", ScoringLocations.C);
+        scoringSecond.addOption("D", ScoringLocations.D);
+        scoringSecond.addOption("E", ScoringLocations.E);
+        scoringSecond.addOption("F", ScoringLocations.F);
+        scoringSecond.addOption("G", ScoringLocations.G);
+        scoringSecond.addOption("H", ScoringLocations.H);
+        scoringSecond.addOption("I", ScoringLocations.I);
+        scoringSecond.addOption("J", ScoringLocations.J);
+        scoringSecond.addOption("K", ScoringLocations.K);
+        scoringSecond.addOption("L", ScoringLocations.L);
+
+        scoringThird = new SendableChooser<>();
+        scoringThird.addOption("A", ScoringLocations.A);
+        scoringThird.addOption("B", ScoringLocations.B);
+        scoringThird.addOption("C", ScoringLocations.C);
+        scoringThird.addOption("D", ScoringLocations.D);
+        scoringThird.addOption("E", ScoringLocations.E);
+        scoringThird.addOption("F", ScoringLocations.F);
+        scoringThird.addOption("G", ScoringLocations.G);
+        scoringThird.addOption("H", ScoringLocations.H);
+        scoringThird.addOption("I", ScoringLocations.I);
+        scoringThird.addOption("J", ScoringLocations.J);
+        scoringThird.addOption("K", ScoringLocations.K);
+        scoringThird.addOption("L", ScoringLocations.L);
+
     }
 
     private void configureBindings() {
@@ -108,9 +154,6 @@ public class RobotContainer {
         joystick.button(14).onTrue(new InstantCommand(() -> drivetrain.sequenceOnTheFlyPaths(ScoringLocations.UPPERHP.value)));
 
         joystick.axisMagnitudeGreaterThan(0, 0.0).or(() -> joystick.axisMagnitudeGreaterThan(1, 0.0).getAsBoolean()).onTrue(new InstantCommand(() -> drivetrain.resetOnTheFly()));
-
-        Field2d field = new Field2d();
-
     }
 
     public Command getAutonomousCommand() {
