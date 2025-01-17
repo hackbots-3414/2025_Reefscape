6cae849678c82488fa801c19d579d3b289e084e3
AkshayKumar41 <akskum700@gmail.com>
2025-01-16 20:29:16 -0500

    updated pic for sim, gotta see if shuffleboard works with custom maps

--- ThriftyTest/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ ThriftyTest/src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -27,6 +27,8 @@ import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -52,6 +54,8 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
 
     private Pose2d estimatedPose = new Pose2d();
     private ApplyRobotSpeeds autoRequest = new ApplyRobotSpeeds().withDriveRequestType(SwerveModule.DriveRequestType.Velocity);
+
+    private Field2d field = new Field2d();
     
     public CommandSwerveDrivetrain(SwerveDrivetrainConstants drivetrainConstants,SwerveModuleConstants<?, ?, ?>... modules) {
         super(drivetrainConstants, modules);
@@ -214,6 +218,8 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
             });
         }
         handleOnTheFly();
+        field.setRobotPose(estimatedPose);
+        SmartDashboard.putData(field);
     }
 
     private void startSimThread() {
