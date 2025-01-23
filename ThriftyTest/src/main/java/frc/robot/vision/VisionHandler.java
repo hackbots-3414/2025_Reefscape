5929fbb84c7292068b19e6a580c7e8043b47cc0e
Nolan <nolanb414@yahoo.com>
2025-01-23 06:33:04 -0500

    fixed imports

--- ThriftyTest/src/main/java/frc/robot/vision/VisionHandler.java
+++ ThriftyTest/src/main/java/frc/robot/vision/VisionHandler.java
@@ -18,7 +18,6 @@ import org.slf4j.LoggerFactory;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
