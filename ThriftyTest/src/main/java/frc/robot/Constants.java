0ea706751d53b71189b6c78f9d1499240096fc87
Nolan <nolanb414@yahoo.com>
2025-01-24 21:16:19 -0500

    minor improvements; lots more constants, and added all 8 cameras.

--- ThriftyTest/src/main/java/frc/robot/Constants.java
+++ ThriftyTest/src/main/java/frc/robot/Constants.java
@@ -1,32 +1,103 @@
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Milliseconds;
 
 import java.util.Map;
 
 import com.pathplanner.lib.config.PIDConstants;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.Time;
+
+/*
+PLEASE READ:
+
+To ensure consistency throughout the code, the same coordinate system is used
+here as is specified in WPILib's documentation.
+
+To read it all, check this out:
+https://docs.wpilib.org/en/stable/docs/software/basic-programming/coordinate-system.html
+
+TL;DR:
+We use NWU axes convention.
+This means that, when viewed from above, the North, West, and then upwards will
+correspond to +X, +Y, and +Z respectively.
+
+Example:
+           +X
+            ^
+            |
+        |-front-|
+        |       |
++Y <--- |       |
+        |       |
+        |-------|
+
+And +Z is upwards, so it wouldn't show here.
+*/
 
 public class Constants {
+    public static class RobotConstants {
+        public static final double k_robotX = Units.inchesToMeters(20.0);
+        public static final double k_robotY = Units.inchesToMeters(20.0);
+
+        public static final double k_cameraOffsetX = k_robotX / 2.0;
+        public static final double k_cameraOffsetY = k_robotY / 2.0;
+        
+        public static final double k_cameraHeight = Units.inchesToMeters(9.0);
+    }
     public static class DriveConstants {
         public static final PIDConstants k_translationPID = new PIDConstants(5.0, 0.0, 0.0);
         public static final PIDConstants k_rotationPID = new PIDConstants(5.0, 0.0, 0.0);
     }
     public static class VisionConstants {
+        // aliases
+        private static final double x = RobotConstants.k_cameraOffsetX;
+        private static final double y = RobotConstants.k_cameraOffsetY;
+        private static final double z = RobotConstants.k_cameraHeight;
+
         // The camera names
         public static Map<String, Transform3d> cameras = Map.ofEntries(
-            Map.entry("Camera", new Transform3d(
-                new Translation3d(-0.52, -0.52, 1.0),
-                new Rotation3d(0,0,Math.PI/6)
+            Map.entry("0", new Transform3d(
+                new Translation3d(x, y, z),
+                new Rotation3d(0, 0, Math.PI/6 /* 30 degrees */)
+            )),
+            Map.entry("1", new Transform3d(
+                new Translation3d(x, y, z),
+                new Rotation3d(0, 0, Math.PI / 3 /* 60 degrees */)
+            )),
+            Map.entry("2", new Transform3d(
+                new Translation3d(-x, y, z),
+                new Rotation3d(0, 0, 2.0 * Math.PI / 3.0)
+            )),
+            Map.entry("3", new Transform3d(
+                new Translation3d(-x, y, z),
+                new Rotation3d(0, 0, 5.0 * Math.PI / 6.0)
+            )),
+            Map.entry("4", new Transform3d(
+                new Translation3d(-x, -y, z),
+                new Rotation3d(0, 0, -5.0 * Math.PI / 6.0)
+            )),
+            Map.entry("5", new Transform3d(
+                new Translation3d(-x, -y, z),
+                new Rotation3d(0, 0, -2.0 * Math.PI / 3.0)
+            )),
+            Map.entry("6", new Transform3d(
+                new Translation3d(x, -y, z),
+                new Rotation3d(0, 0, -Math.PI / 3.0)
+            )),
+            Map.entry("7", new Transform3d(
+                new Translation3d(x, -y, z),
+                new Rotation3d(0, 0, -Math.PI / 6.0)
             ))
-            // Map.entry("Front Camera", new Transform3d(
-            //     new Translation3d(0.52, 0.52, 1.0),
-            //     new Rotation3d(0, 0, Math.PI)
-            // ))
         );
         // The tick time for each pose estimator to run
         public static final double k_periodic = 0.02;
@@ -48,6 +119,15 @@ public class Constants {
         public static final double k_ambiguityMultiplier = 0.4;
         public static final double k_ambiguityShifter = 0.2;
         public static final double k_targetMultiplier = 10;
+        // Stats about the camera for simulation
+        public static final int k_resWidth = 320;
+        public static final int k_resHeight = 240;
+        public static final Rotation2d k_fov = Rotation2d.fromDegrees(70.0);
+        // Simulated error:
+        public static final Time k_avgLatency = Milliseconds.of(18);
+        public static final Time k_latencyStdDev = Milliseconds.of(5);
+        public static final double k_avgErr = 0.03;
+        public static final double k_errStdDev = 0.02;
     }
     public static class FieldConstants {
         public static final Distance k_fieldWidth = Meters.of(8.05);
--- ThriftyTest/src/main/java/frc/robot/vision/VisionHandler.java
+++ ThriftyTest/src/main/java/frc/robot/vision/VisionHandler.java
@@ -1,6 +1,7 @@
 package frc.robot.vision;
 
 import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Milliseconds;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -8,19 +9,16 @@ import java.util.List;
 import java.util.Map;
 
 import org.photonvision.PhotonCamera;
-import org.photonvision.estimation.TargetModel;
 import org.photonvision.simulation.PhotonCameraSim;
 import org.photonvision.simulation.SimCameraProperties;
 import org.photonvision.simulation.VisionSystemSim;
-import org.photonvision.simulation.VisionTargetSim;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
@@ -39,6 +37,8 @@ public class VisionHandler implements AutoCloseable {
     private SimCameraProperties m_simProps = new SimCameraProperties();
 
     private Field2d m_field;
+    
+    private List<TimestampedPoseEstimate> m_lastEstimates = new ArrayList<>();
 
     public VisionHandler(CommandSwerveDrivetrain drivetrain) {
         m_drivetrain = drivetrain;
@@ -48,25 +48,47 @@ public class VisionHandler implements AutoCloseable {
             m_logger.error("could not load april tag resource file");
             System.exit(1);
         }
-        m_simProps.setCalibration(320, 240, new Rotation2d(Degrees.of(70)));
-        m_simProps.setAvgLatencyMs(18);
-        m_simProps.setLatencyStdDevMs(5);
-        m_simProps.setCalibError(0.03, 0.02);
+        setupProps();
         setupCameras();
-        m_notifier = new Notifier(() -> {
-            for (Runnable estimator : m_estimators) {
-                estimator.run();
-            }
-            m_visionSim.update(drivetrain.getPose());
-        });
-        m_visionSim.addVisionTargets(
-            new VisionTargetSim(
-                new Pose3d(1, 1, 3, new Rotation3d()),
-                new TargetModel(2, 2, 2)
-            )
-        );
+        m_notifier = new Notifier(this::updateEstimators);
         m_field = m_visionSim.getDebugField();
         SmartDashboard.putData("April Tag Debug Field", m_field);
+    }
+
+    private void setupAprilTagField() throws IOException {
+        AprilTagFieldLayout tagLayout = AprilTagFieldLayout.loadFromResource(
+            AprilTagFields.k2025Reefscape.m_resourceFile
+        );
+        m_visionSim.addAprilTags(tagLayout);
+    }
+
+    /**
+     * Sets up the simulated camera properties with values that should
+     * reflect the real world situation.
+     */
+    private void setupProps() {
+        m_simProps.setCalibration(
+            VisionConstants.k_resWidth,
+            VisionConstants.k_resHeight,
+            VisionConstants.k_fov
+        );
+        m_simProps.setAvgLatencyMs(
+            VisionConstants.k_avgLatency.in(Milliseconds)
+        );
+        m_simProps.setLatencyStdDevMs(
+            VisionConstants.k_latencyStdDev.in(Milliseconds)
+        );
+        m_simProps.setCalibError(
+            VisionConstants.k_avgErr,
+            VisionConstants.k_errStdDev
+        );
+    }
+
+    private void updateEstimators() {
+        for (SingleInputPoseEstimator estimator : m_estimators) {
+            estimator.run();
+        }
+        m_visionSim.update(m_drivetrain.getPose());
     }
 
     public void startThread() {
@@ -88,23 +110,22 @@ public class VisionHandler implements AutoCloseable {
                 // competition use. it will boink the roborio's cpu :(
                 simCamera.enableDrawWireframe(true);
             }
+            m_visionSim.getCameraPose(simCamera);
+            // Transform2d robotToCamera2d = new Transform2d(
+            //     robotToCamera.getX(),
+            //     robotToCamera.getY(),
+            //     robotToCamera.getRotation().toRotation2d()
+            // );
             SingleInputPoseEstimator estimator = new SingleInputPoseEstimator(
                 realCamera,
                 robotToCamera,
                 (TimestampedPoseEstimate estimate) -> {
-                    m_field.getObject(cameraName).setPose(estimate.pose());
+                    m_lastEstimates.add(estimate);
                     m_drivetrain.addPoseEstimate(estimate);
                 }
             );
             m_estimators.add(estimator);
         }
-    }
-
-    public void setupAprilTagField() throws IOException {
-        AprilTagFieldLayout tagLayout = AprilTagFieldLayout.loadFromResource(
-            AprilTagFields.k2025Reefscape.m_resourceFile
-        );
-        m_visionSim.addAprilTags(tagLayout);
     }
 
     public void close() {
