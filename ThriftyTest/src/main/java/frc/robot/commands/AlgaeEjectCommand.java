d020e5488ed49cc8f4b6b75786c0ad962691fd2b
Flowers Computer <hackbots@flowers.hackbots>
2025-02-05 18:42:46 -0500

    Algae Eject Command, Teleop Command Reset Initialize

---
+++ ThriftyTest/src/main/java/frc/robot/commands/AlgaeEjectCommand.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.AlgaeRollers;
+
+public class AlgaeEjectCommand extends Command {
+  private AlgaeRollers rollers;
+  public AlgaeEjectCommand(AlgaeRollers rollers) {
+   this.rollers = rollers;
+  }
+
+  @Override
+  public void initialize() {
+    rollers.ejectAlgae();
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    rollers.stopMotor();
+  }
+}
