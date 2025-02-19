6c72b69db2a8613cdfc6a832b4f08927790932e4
Nolan Barker <107814443+TheRekrab@users.noreply.github.com>
2025-02-19 16:09:13 -0500

    Updated PID for comp bot 2/17 (No elevator)

--- 2025_Reefscape/src/main/java/frc/robot/generated/TunerConstants.java
+++ 2025_Reefscape/src/main/java/frc/robot/generated/TunerConstants.java
@@ -24,14 +24,14 @@ public class TunerConstants {
     // The steer motor uses any SwerveModule.SteerRequestType control request with the
     // output type specified by SwerveModuleConstants.SteerMotorClosedLoopOutput
     private static final Slot0Configs steerGains = new Slot0Configs()
-        .withKP(100).withKI(0).withKD(0.5)
-        .withKS(0.1).withKV(3.1).withKA(0)
+        .withKP(1.6826).withKI(0).withKD(0)
+        .withKS(0.27075).withKV(2.902).withKA(0.092378) //  All steer Values were really bad, but this was the best one
         .withStaticFeedforwardSign(StaticFeedforwardSignValue.UseClosedLoopSign);
     // When using closed-loop control, the drive motor uses the control
     // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
     private static final Slot0Configs driveGains = new Slot0Configs()
-        .withKP(0.1).withKI(0).withKD(0)
-        .withKS(0).withKV(0.124);
+        .withKP(0.067725).withKI(0).withKD(0)
+        .withKS(0.198095).withKV(0.12131).withKA(0.003469);
 
     // The closed-loop output type to use for the steer motors;
     // This affects the PID/FF gains for the steer motors
