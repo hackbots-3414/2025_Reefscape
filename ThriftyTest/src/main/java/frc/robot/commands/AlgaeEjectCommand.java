03052ddda3054e00c7510af8fefb3d10672b943b
Nolan Barker <nolanb414@yahoo.com>
2025-02-23 13:30:27 -0500

    there was more...

---
+++ ThriftyTest/src/main/java/frc/robot/commands/AlgaeEjectCommand.java
@@ -0,0 +1,23 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.AlgaeRollers;
+
+public class AlgaeEjectCommand extends Command {
+  private final AlgaeRollers m_rollers;
+  
+  public AlgaeEjectCommand(AlgaeRollers rollers) {
+   m_rollers = rollers;
+   addRequirements(rollers);
+  }
+
+  @Override
+  public void initialize() {
+    m_rollers.ejectAlgae();
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    m_rollers.stop();
+  }
+}
