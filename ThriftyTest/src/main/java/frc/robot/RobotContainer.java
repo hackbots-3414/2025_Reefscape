5e6d0a6ece7d9686f65f0285912b16863142b7cb
AkshayKumar41 <akskum700@gmail.com>
2025-02-26 13:27:35 -0500

    added hard coded paths, fixed super auton more

--- ThriftyTest/src/main/java/frc/robot/RobotContainer.java
+++ ThriftyTest/src/main/java/frc/robot/RobotContainer.java
@@ -394,8 +394,6 @@ public class RobotContainer {
 
     // ********** AUTONOMOUS **********
 
-    // private final SendableChooser<Command> autoChooser;
-
     private final ArrayList<SendableChooser<Pose2d>> scoringLocationsChooser = new ArrayList<>();
     private final SendableChooser<Pose2d> pickupLocation = new SendableChooser<>();
     private final ArrayList<SendableChooser<Supplier<Command>>> scoringHeightsChooser = new ArrayList<>();
