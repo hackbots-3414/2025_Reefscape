1fb0c959413112998f680869a68979d27e7fa96b
Comp Laptop <team3414hackbots@gmail.com>
2025-04-03 17:25:58 -0400

    vision log tool -> log destroyer

--- logtool.sh
+++ logtool.sh
@@ -4,11 +4,11 @@
 
 DOWNLOAD_PATH=~/extracted_`date +%Y-%m-%d_%H-%M`
 
-echo "Saving logs to $DOWNLOAD_PATH"
+# echo "Saving logs to $DOWNLOAD_PATH"
 
-mkdir -p $DOWNLOAD_PATH
+# mkdir -p $DOWNLOAD_PATH
 
-scp lvuser@10.34.14.2:~/logs/* $DOWNLOAD_PATH
+# scp lvuser@10.34.14.2:~/logs/* $DOWNLOAD_PATH
 
 # NUKE IT
 ssh lvuser@10.34.14.2 "rm ~/logs/*"
