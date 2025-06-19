c5c4aeff5bc5d6caf25b32c63f9383683f9eeae7
Nolan Barker <nolanb414@yahoo.com>
2025-06-19 11:13:13 -0600

    Add more realistic elevator simulation

--- ThriftyTest/src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
+++ ThriftyTest/src/main/java/frc/robot/subsystems/elevator/ElevatorIOSim.java
@@ -1,16 +1,18 @@
 package frc.robot.subsystems.elevator;
 
 public class ElevatorIOSim implements ElevatorIO {
-  private double m_position = Double.NaN;
+  private double m_position = 0;
+  private double m_reference = 0;
 
   public void updateInputs(ElevatorIOInputs inputs) {
+    m_position = (m_position + m_reference) / 2.0;
     inputs.position = m_position;
-    inputs.reference = m_position;
-    inputs.zeroCANrangeDetected = m_position == 0;
+    inputs.reference = m_reference;
+    inputs.zeroCANrangeDetected = m_position < 1e-3;
   }
 
   public void setPosition(double position) {
-    m_position = position; // that's an awesome elevator! it's instant!
+    m_reference = position; // that's an awesome elevator! it's instant!
   }
 
   public void setVoltage(double voltage) {}
