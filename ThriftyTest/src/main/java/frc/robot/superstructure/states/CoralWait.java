d22f92b00471fc4a51e454f1571f3e81319698cf
Comp Laptop <team3414hackbots@gmail.com>
2025-08-04 18:22:08 -0400

    Add small wait to CoralWait to avoid mistakes

--- ThriftyTest/src/main/java/frc/robot/superstructure/states/CoralWait.java
+++ ThriftyTest/src/main/java/frc/robot/superstructure/states/CoralWait.java
@@ -8,11 +8,14 @@ import frc.robot.superstructure.Superstructure.Subsystems;
 public class CoralWait implements EnterableState {
   /**
    * A state that puts the robot into a "waiting" state until a coral can be acquired.
-   * The state is NOT a coral itake state, however, and does not actually change any mechanisms
+   * The state is NOT a coral itake state, however, and does not actually change any mechanisms.
+   * This also will wait 0.1s first before anything else happens to avoid any mistakes (coral leaving scoring mechanism).
    */
   public CoralWait() {}
 
   public Command build(Subsystems subsystems) {
-    return Commands.waitUntil(subsystems.coral().present());
+    return Commands.sequence(
+      Commands.waitSeconds(0.1),
+      Commands.waitUntil(subsystems.coral().present()));
   }
 }
